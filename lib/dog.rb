36f077591fc34ba8df6323c6421aa4dad2133e75
Jessica Gamez <jpgamez1789@icloud.com>
2020-05-17 21:30:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+  
+  def initialize (name, owner)
+    @name = name
+    @mood = "nervous"
+    self.owner = owner
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end
+end# code goes here
 end
